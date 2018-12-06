e7c92524d36fc9b2c620f218c10756521970be2d
Javier Castillo Delgado <44399661+javcasdel@users.noreply.github.com>
2018-12-07 00:00:04 +0100

    Feature: añadida estructura básica de ley d'Hondt; #1
    Se ha añadido la multiplexión básica de diferentes tipos de recuento en las vistas de postprocesado.

--- decide/postproc/views.py
+++ decide/postproc/views.py
@@ -16,6 +16,9 @@ class PostProcView(APIView):
         out.sort(key=lambda x: -x['postproc'])
         return Response(out)
 
+    def dhondt(self, options):
+        return Response({})
+
     def post(self, request):
         """
          * type: IDENTITY | EQUALITY | WEIGHT
@@ -34,5 +37,8 @@ class PostProcView(APIView):
 
         if t == 'IDENTITY':
             return self.identity(opts)
+
+        else if t == 'DHONDT':
+            return self.dhondt(opts)            
 
         return Response({})
