427dbc7542104bd5ce0051bc0acec2f47860928d
Cristian Rodriguez EGC <cristian96-11@hotmail.es>
2018-12-12 21:57:12 +0100

    [DOCS] Pruebas simetricas 2
    Se ha añadido una explicación en profundidad de como funciona el algoritmo de cifrado AES

--- decide/mixnet/test_simetrico.py
+++ decide/mixnet/test_simetrico.py
@@ -14,15 +14,28 @@ print(msg)
 from Crypto.Cipher import AES 
 import binascii,os
 
+
 texto = "CristianRodrigu "
 key = "00112233445566778899aabbccddeeff"
 iv = os.urandom(16)
 aes_mode = AES.MODE_CBC
 obj = AES.new(key, aes_mode, iv)
+#Cifrado: 
+# 1- El vector de inicialización iv entra al criptosistema con la clave K, esto nos resultará en un S1
+# 2- Se operará S1 XOR texto[1]  resultará en el criptosistema 1 encriptado = C1
+# 3- C1 vuelve a entrar al cipher con la clave y se obtiene un s2
+# 4- Este s2 se vuelve a realizar una XOR con texto[2]
+#El ciclo se repite hasta encriptar todo el mensaje. Es un cifrado en secuencia.
+#Secuencia resultante = b'\x16\x18\xd9\xdcv6\x81\xf8$\xe8\xbd\x0e\x15\x1cKB'
 ciphertext = obj.encrypt(texto)
-
 print(ciphertext)
 
+#Descifrado:
+# A partir de los bloques obtenidos del cifrado operaremos de la siguiente manera
+# 1- iv (vector de inicialización) con la clave k entran al criptosistema => S1
+# 2- S1 XOR c1(bloque cifrado) = mensaje en claro
+# 3- C1 con k entran al criptosistema => S2
+# 4- S2 XOR c2 = mensaje en claro 
 cipher = AES.new(key, aes_mode, iv)
 plaintext = cipher.decrypt(ciphertext)
 print(plaintext)    
