19230e2f802da1c378502e02c56d488b69509a07
mruwzum <ma.antolinbermudez@gmail.com>
2019-01-08 16:36:57 +0100

    [FIX] Added password of db to travis-ci config

--- decide/local_settings.travis.py
+++ decide/local_settings.travis.py
@@ -32,6 +32,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
+        'PASSWORD': 'postgres',
         'HOST': 'localhost',
         'PORT': '5432',
     }
